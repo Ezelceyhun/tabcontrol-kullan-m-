79379bb637f9092ac479545daf596fc251e5b0d5
Ceyhun <47317865+Ezelceyhun@users.noreply.github.com>
2024-02-29 11:52:59 +0300

    güncellendi

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -45,7 +45,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(766, 329);
+            this.ClientSize = new System.Drawing.Size(766, 308);
             this.ControlBox = false;
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
