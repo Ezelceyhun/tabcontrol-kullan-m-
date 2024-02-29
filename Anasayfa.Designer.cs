9a7a78cf23242b7f484b08b8543f423c29e7a854
Ceyhun <47317865+Ezelceyhun@users.noreply.github.com>
2024-02-29 11:33:12 +0300

    Dosyalar Eklendi

---
+++ Anasayfa.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace tabcontrol_kullanımı
+{
+    partial class Anasayfa
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Location = new System.Drawing.Point(12, 23);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(766, 329);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(784, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Sekme 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(784, 79);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Sekme 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(784, 133);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 40);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Sekme 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(784, 190);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 40);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Sekme 4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Transparent;
+            this.button6.BackgroundImage = global::tabcontrol_kullanımı.Properties.Resources.logout_FILL0_wght400_GRAD0_opsz24;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button6.Location = new System.Drawing.Point(840, 248);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(47, 42);
+            this.button6.TabIndex = 6;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Transparent;
+            this.button5.BackgroundImage = global::tabcontrol_kullanımı.Properties.Resources.close_FILL0_wght400_GRAD0_opsz24;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Image = global::tabcontrol_kullanımı.Properties.Resources.close_FILL0_wght400_GRAD0_opsz24;
+            this.button5.Location = new System.Drawing.Point(784, 247);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(47, 42);
+            this.button5.TabIndex = 5;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button7.Location = new System.Drawing.Point(784, 296);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(103, 57);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Tümünü Kapat";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Anasayfa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 382);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Name = "Anasayfa";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TabControl Kullanımı";
+            this.Load += new System.EventHandler(this.Anasayfa_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Anasayfa.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace tabcontrol_kullanımı
+{
+    public partial class Anasayfa : Form
+    {
+        public Anasayfa()
+        {
+            InitializeComponent();
+            this.IsMdiContainer = true;
+        }
+        Form1 form1 = new Form1();
+        Form2 form2 = new Form2();
+        Form3 form3 = new Form3();
+        Form4 form4 = new Form4();
+        //TabPage myTabPage = new TabPage();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string title = "Sekme " + (tabControl1.TabCount + 1).ToString();
+            TabPage myTabPage = new TabPage();      
+            if (tabControl1.TabCount == 0)
+            {
+                
+                form1.MdiParent = this;
+                myTabPage.Text = title;
+                tabControl1.TabPages.Add(myTabPage);
+                tabControl1.SelectedIndex = 0;
+                form1.Show();
+                myTabPage.Controls.Add(form1);              
+            }
+            else if(myTabPage != null)
+            {
+                tabControl1.SelectedIndex = 0;
+                form1.Show();
+            }
+            else
+            {
+                MessageBox.Show("hata");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string title = "Sekme " + (tabControl1.TabCount + 1).ToString();
+            TabPage myTabPage = new TabPage();
+            if (tabControl1.TabCount == 1)
+            {
+                
+                form2.MdiParent = this;
+                myTabPage.Text = title;
+                tabControl1.TabPages.Add(myTabPage);
+                tabControl1.SelectedIndex = 1;
+                form2.Show();
+                myTabPage.Controls.Add(form2);
+            }
+            else if (myTabPage != null)
+            {
+                tabControl1.SelectedIndex = 1;
+                form2.Show();
+            }
+            else
+            {
+                MessageBox.Show("hata");
+            }
+        }
+
+        private void Anasayfa_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string title = "Sekme " + (tabControl1.TabCount + 1).ToString();
+            TabPage myTabPage = new TabPage();
+            if (tabControl1.TabCount == 2)
+            {
+
+                form3.MdiParent = this;
+                myTabPage.Text = title;
+                tabControl1.TabPages.Add(myTabPage);
+                tabControl1.SelectedIndex = 2;
+                form3.Show();
+                myTabPage.Controls.Add(form3);
+            }
+            else if (myTabPage != null)
+            {
+                tabControl1.SelectedIndex = 2;
+                form3.Show();
+            }
+            else
+            {
+                MessageBox.Show("hata");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string title = "Sekme " + (tabControl1.TabCount + 1).ToString();
+            TabPage myTabPage = new TabPage();
+            if (tabControl1.TabCount == 3)
+            {
+
+                form4.MdiParent = this;
+                myTabPage.Text = title;
+                tabControl1.TabPages.Add(myTabPage);
+                tabControl1.SelectedIndex = 3;
+                form4.Show();
+                myTabPage.Controls.Add(form4);
+            }
+            else if (myTabPage != null)
+            {
+                tabControl1.SelectedIndex = 3;
+                form4.Show();
+            }
+            else
+            {
+                MessageBox.Show("hata");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if(tabControl1.SelectedIndex == 0)
+            {
+                form1.Hide();
+            }
+            else if(tabControl1.SelectedIndex == 1) 
+            {
+                form2.Hide();
+            }
+            else if(tabControl1.SelectedIndex == 2)
+            {
+                form3.Hide();
+            }
+            else if(tabControl1.SelectedIndex == 3)
+            {
+                form4.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Kapatmak İstediğiniz Sekmeyi Seçin!");
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            form1.Hide();
+            form2.Hide();
+            form3.Hide();
+            form4.Hide();
+        }
+    }
+}
